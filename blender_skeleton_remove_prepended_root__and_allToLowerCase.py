6f1fcf78bd349a2aa5735137ec3360797d8de9c9
J.R.I.B.-Wein <Radagast@dragontale.de>
2014-03-15 18:24:49 +0100

    added python script for fixing armature bone names for biped skeleton to match pyrogenesis' standard_skeleton.

---
+++ blender_skeleton_remove_prepended_root__and_allToLowerCase.py
@@ -0,0 +1,32 @@
+# Blender Python script - Tested with v2.70, should work with 2.6+.
+
+import bpy
+
+#------- FUNCTIONS ------------------------------------------------------------#
+
+#ACT
+def act():#context):
+    #a = context.armature
+    #print(a)
+    #if a is None:
+    a = bpy.data.armatures['Armature']    #TODO better use 0 as index?
+    remove_pattern = "root_"
+    if not a:
+        print("Armature still not found!")
+        return False
+    for b in a.bones:
+        old_name = b.name
+        print(b.name)
+        if b.name.find(remove_pattern) != -1:
+            print('found prepended ' + remove_pattern)
+            b.name = b.name.replace(remove_pattern, "")
+        b.name = b.name.lower()
+        if (old_name != b.name):
+            print('bone renamed to:' + b.name)
+        else:
+            print('nothing to do - report bug in blenderartists or wildfireforums if you think something is wrong.\r\nhttp://www.wildfiregames.com/forum/index.php?showtopic=15552&page=19#entry286903')
+        print("\r\n")
+    return True
+
+
+act()#context)
